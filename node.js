fc97eb53f4635dc0a3ff96ff68b7c728aeb21a92
shark4514css <2383927625@qq.com>
2020-06-01 14:07:32 +0800

    Create node.js

---
+++ node.js
@@ -0,0 +1,6 @@
+1. nodejs里面运行javascript和chrome里面运行有什么不同？
+chrome浏览器运行的是同样的javascript引擎和模型
+在nodejs里面写js和在chrome里面几乎没有区别
+不同的是nodejs里面没有浏览器的api window，document，增加了nodejs专属api，文件系统，进程等等
+所以在chorme里面写js控制的是浏览器，nodejs希望你写js控制整个计算机
+2. nodejs适合什么项目
